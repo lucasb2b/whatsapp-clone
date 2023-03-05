36bd9a4303abeb128f2ca02771d2d484a80c6439
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-03-05 18:54:42 -0300

    Build the chat list item

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ChatListItem from './src/components/ChatListItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      
+    <ChatListItem />
+
       <StatusBar style="auto" />
     </View>
   );
